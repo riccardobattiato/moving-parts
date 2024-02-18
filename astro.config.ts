bd8758d2253d6db274a9d26dd11a184ddbfe665a
riccardobattiato <rbattiato@outlook.it>
2024-02-18 11:55:03 +0100

    feat: remark reading time

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs"
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
@@ -19,6 +20,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [
       remarkToc,
+      remarkReadingTime,
       [
         remarkCollapse,
         {
